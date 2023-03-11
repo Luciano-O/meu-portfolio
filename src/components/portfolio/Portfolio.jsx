7fe41d9e7d76885c417d86e2f6ca971c436c97dc
Luciano <oliveiraluck78@gmail.com>
2023-03-10 22:38:16 -0300

    Portfolio image fix

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/portfolio1.png";
-import IMG2 from "../../assets/portfolio2.png";
+import IMG2 from "../../assets/portfolio2.jpg";
 import IMG3 from "../../assets/portfolio3.jpg";
 
 const Portfolio = () => {
